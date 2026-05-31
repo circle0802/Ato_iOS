826315d7f32c002bec33eab48d95c308d083e3d8
원이 <hawon9781@gmail.com>
2026-05-31 18:57:07 +0900

    프로젝트 생성 및 앱 아이콘 설정

---
+++ Ato/Ato/Resources/AtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtoApp.swift
+//  Ato
+//
+//  Created by hawon on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct AtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
---
+++ Ato/Ato/Sources/SplashView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ato
+//
+//  Created by hawon on 5/31/26.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SplashView()
+}
